17b196a6ccab24dfabcf0dc0e3ead78cc9541ef6
Ashvini <ashvini.deshmukh@lntinfotech.com>
2022-11-28 11:05:14 +0530

    table seach changes

--- src/pages/profile/components/table-search/table-search.js
+++ src/pages/profile/components/table-search/table-search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchIcon from '@mui/icons-material/Search';
 import {
@@ -15,7 +16,6 @@ import {
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
 
-import EXPORTICON from '../../../../assets/images/ico-logout.svg';
 import SearchableDropdown from '../../../../components/autocomplete/searchable-dropdown';
 import { verboseLog } from '../../../../config/debug';
 import { Button, TextField } from '../../../../ui/core';
@@ -32,7 +32,7 @@ export function TableSearch() {
   };
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const id = open ? 'table-search-popover' : undefined;
 
   const loggedInUserType = useSelector((state) => state.login.loggedInUserType);
   const {
@@ -57,10 +57,11 @@ export function TableSearch() {
   return (
     <Box data-testid="table-search">
       <Grid container sx={{ p: 0 }}>
-        <Grid item md={4} xs={12} mb={1}>
-          <Grid item md={8} xs={8}>
+        <Grid item md={2} xs={12} mb={1}>
+          <Grid item md={12} xs={12}>
             <TextField
-              inputProps={{ maxLength: 100, sx: { height: 12 } }}
+              data-testid="freesearch"
+              inputProps={{ maxLength: 100 }}
               fullWidth={true}
               id="outlined-basic"
               variant="outlined"
@@ -82,88 +83,101 @@ export function TableSearch() {
             />
           </Grid>
         </Grid>
-        <Grid item md={8} xs={12} mb={1}>
-          <Box sx={{ display: 'flex' }}>
-            <TextField
-              inputProps={{ maxLength: 100 }}
-              fullWidth={true}
-              id="outlined-basic"
-              variant="outlined"
-              type="text"
-              name="filterByName"
-              required="false"
-              placeholder={'Filyer by Name'}
-              // margin="dense"
-              defaultValue={getValues().filterByName}
-              error={errors.filterByName?.message}
-              {...register('filterByName')}
-            />
-            <TextField
-              sx={{ ml: 1 }}
-              inputProps={{ maxLength: 100, sx: { height: 12 } }}
-              fullWidth={true}
-              id="outlined-basic"
-              variant="outlined"
-              type="text"
-              name="filterByRegNo"
-              required="false"
-              placeholder={'Filyer by Registration No.'}
-              // margin="dense"
-              defaultValue={getValues().filterByRegNo}
-              error={errors.filterByRegNo?.message}
-              {...register('filterByRegNo')}
-            />
+        <Grid item md={10} xs={12} mb={1}>
+          <Grid container item spacing={1} justifyContent="flex-end">
+            <Grid item md={3} xs={12}>
+              <TextField
+                data-testid="filterByName"
+                inputProps={{ maxLength: 100 }}
+                fullWidth={true}
+                id="outlined-basic"
+                variant="outlined"
+                type="text"
+                name="filterByName"
+                required="false"
+                placeholder={'Filyer by Name'}
+                // margin="dense"
+                defaultValue={getValues().filterByName}
+                error={errors.filterByName?.message}
+                {...register('filterByName')}
+              />
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <TextField
+                data-testid="filterByRegNo"
+                // sx={{ ml: 1 }}
+                inputProps={{ maxLength: 100 }}
+                fullWidth={true}
+                id="outlined-basic"
+                variant="outlined"
+                type="text"
+                name="filterByRegNo"
+                required="false"
+                placeholder={'Filyer by Registration No.'}
+                // margin="dense"
+                defaultValue={getValues().filterByRegNo}
+                error={errors.filterByRegNo?.message}
+                {...register('filterByRegNo')}
+              />
+            </Grid>
             {(loggedInUserType === 'College' || loggedInUserType === 'NMC') && (
-              <Box sx={{ ml: 1 }}>
-                <SearchableDropdown
-                  sx={{ height: 8 }}
-                  name="RegistrationCouncil"
-                  items={[{ id: 1, name: 'first' }]}
-                />
-              </Box>
+              <Grid item md={3} xs={12}>
+                <Box>
+                  <SearchableDropdown
+                    sx={{ height: 8 }}
+                    name="RegistrationCouncil"
+                    items={[{ id: 1, name: 'first' }]}
+                  />
+                </Box>
+              </Grid>
             )}
-            <Button
-              size="small"
-              variant="contained"
-              sx={{ ml: 1, height: '48px' }}
-              onClick={handleSubmit(onClickFilterButtonHandler)}
-            >
-              Filter
-            </Button>
-            <Button
-              size="small"
-              variant="contained"
-              sx={{ ml: 1 }}
-              endIcon={<KeyboardArrowDownIcon />}
-              onClick={handleClick}
-            >
-              {' '}
-              <img src={EXPORTICON} alt="NHA English Logo" />
-            </Button>
-            <Popover
-              id={id}
-              open={open}
-              anchorEl={anchorEl}
-              onClose={handleClose}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-            >
-              <List>
-                <ListItem disablePadding>
-                  <ListItemButton disablePadding>
-                    <ListItemText primary="Export as xlsx" />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton onClick={handleClose}>
-                    <ListItemText primary="Export as csv" />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-            </Popover>
-          </Box>
+
+            <Grid item md={1} xs={12}>
+              <Button
+                data-testid="filterButton"
+                sx={{ padding: '13px 10px' }}
+                variant="contained"
+                onClick={handleSubmit(onClickFilterButtonHandler)}
+              >
+                Filter
+              </Button>
+            </Grid>
+            <Grid item md={1} xs={12}>
+              <Button
+                data-testid="exportButton"
+                sx={{ padding: '17px 10px' }}
+                variant="contained"
+                endIcon={<KeyboardArrowDownIcon />}
+                onClick={handleClick}
+                startIcon={
+                  <FileUploadOutlinedIcon sx={{ fontSize: '26px', transform: 'rotate(90deg)' }} />
+                }
+              ></Button>
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+              >
+                <List>
+                  <ListItem disablePadding>
+                    <ListItemButton disablePadding>
+                      <ListItemText primary="Export as xlsx" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem disablePadding>
+                    <ListItemButton onClick={handleClose}>
+                      <ListItemText primary="Export as csv" />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </Popover>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
