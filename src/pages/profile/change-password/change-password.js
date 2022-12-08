d24f81a603241ee6c51bd907b49807b5c13b7e96
mohithkschowhan <Chowhan.MohithKumarSingh@lntinfotech.com>
2022-12-08 19:14:38 +0530

    CS-343: reactivation licence

--- src/pages/profile/change-password/change-password.js
+++ src/pages/profile/change-password/change-password.js
@@ -4,10 +4,6 @@ import { useForm } from 'react-hook-form';
 
 import { TextField } from '../../../ui/core';
 
-// import { TextField } from '../../ui/core';
-
-// import { TextField } from '../../../ui/core';
-
 const ChangePassword = () => {
   const {
     register,
