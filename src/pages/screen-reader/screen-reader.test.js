eb3ee4bd400433b143e0de358420f62b97fae108
laxmans-lti <laxman.sutar@lntinfotech.com>
2022-12-13 18:50:04 +0530

    test cases added

--- src/pages/screen-reader/screen-reader.test.js
+++ src/pages/screen-reader/screen-reader.test.js
@@ -1,7 +1,16 @@
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-import ScreenReader from './index';
+import { render, screen } from '@testing-library/react';
 
-test('renders', () => {
+import { ScreenReader } from './index';
+
+test('Screen Reader component snapshot', () => {
+  const { container } = render(<ScreenReader />);
+  expect(container).toMatchSnapshot();
+});
+
+test('Information related to the various screen readers', () => {
   render(<ScreenReader />);
+  const TextElement = screen.getByTestId('infoId');
+  expect(TextElement).toBeInTheDocument();
 });
