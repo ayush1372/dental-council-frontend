69462e8f15f60827cc506a1aacc226d14a77e51a
krishnakanth <Sanka.Krishnakanth@lntinfotech.com>
2022-12-20 13:22:55 +0530

    Added api integration for districts

--- src/store/actions/menu-list-actions.js
+++ src/store/actions/menu-list-actions.js
@@ -1,7 +1,7 @@
 import { API } from '../../api/api-endpoints';
 import { GET } from '../../constants/requests';
 import { useAxiosCall } from '../../hooks/use-axios';
-import { getStates } from '../reducers/menu-lists-reducer';
+import { getDistricts, getStates } from '../reducers/menu-lists-reducer';
 
 export const getStatesList = () => async (dispatch) => {
   return await new Promise((resolve, reject) => {
@@ -11,6 +11,22 @@ export const getStatesList = () => async (dispatch) => {
     })
       .then((response) => {
         dispatch(getStates(response.data));
+        return resolve(response);
+      })
+      .catch((error) => {
+        return reject(error);
+      });
+  });
+};
+
+export const getDistrictList = () => async (dispatch) => {
+  return await new Promise((resolve, reject) => {
+    useAxiosCall({
+      method: GET,
+      url: API.menuLists.districts,
+    })
+      .then((response) => {
+        dispatch(getDistricts(response.data));
         return resolve(response);
       })
       .catch((error) => {
