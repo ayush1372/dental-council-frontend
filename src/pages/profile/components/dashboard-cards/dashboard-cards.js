7ff4fc520aaa44dd0b14df688f7b67dc8f826be7
Ashvini <ashvini.deshmukh@lntinfotech.com>
2023-02-01 13:28:08 +0530

    dashboard card integartion

--- src/pages/profile/components/dashboard-cards/dashboard-cards.js
+++ src/pages/profile/components/dashboard-cards/dashboard-cards.js
@@ -15,11 +15,11 @@ import DashboardControlledTable from '../dashboard-controlled-table/dashboard-co
 
 export default function Dashboard() {
   const theme = useTheme();
-  const loggedInUserType = useSelector((state) => state.common.loggedInUserType);
+  // const loggedInUserType = useSelector((state) => state.common.loggedInUserType);
   const { count } = useSelector((state) => state.dashboard);
 
   // eslint-disable-next-line no-console
-  console.log('count', count?.data, count?.data['hp_registration_requests']);
+  console.log('count', count, count?.data['hp_registration_requests']);
 
   const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
@@ -29,120 +29,175 @@ export default function Dashboard() {
     cursor: 'pointer',
   }));
 
-  let blankDashboard = {
-    'Registration Request': [
-      {
-        name: 'Total Registration Requests',
-        value: 0,
-      },
-      {
-        name: 'Pending',
-        value: 0,
-      },
-      {
-        name: 'Verified',
-        value: 0,
-      },
-      {
-        name: 'Query Raised',
-        value: 0,
-      },
-      {
-        name: 'Rejected',
-        value: 0,
-      },
-    ],
-    'Updation Request': [
-      {
-        name: 'Total Updation Request',
-        value: 0,
-      },
-      {
-        name: 'Update Request Received',
-        value: 0,
-      },
-      {
-        name: 'Update Request Approved',
-        value: 0,
-      },
-      {
-        name: 'Query Raised on Update Request',
-        value: 0,
-      },
-      {
-        name: 'Update Request Rejected',
-        value: 0,
-      },
-    ],
+  // mapping BE keys -> card titles on FE
+  let registrationRequestMapper = {
+    'Total HP Registration Requests': 'Total Registration Requests',
+    Rejected: 'Rejected',
+    Approved: 'Approved',
+    'Query Raised': 'Query Raised',
+    Pending: 'Pending',
+  };
+  let updationRequestMapper = {
+    'Total HP Modification Requests': 'Total Updation Requests',
+    Rejected: 'Rejected',
+    Approved: 'Approved',
+    'Query Raised': 'Query Raised',
+    Pending: 'Pending',
   };
 
-  if (loggedInUserType === 'NMC' || loggedInUserType === 'SMC') {
-    blankDashboard = Object.assign(blankDashboard, {
-      'Suspension Request': [
-        {
-          name: 'Total Suspension Request',
-          value: 0,
-        },
-        {
-          name: 'Temporary Suspension Request Received',
-          value: 0,
-        },
-        {
-          name: 'Temporary Suspension Approved',
-          value: 0,
-        },
-        {
-          name: 'Permanent Suspension Request Received',
-          value: 0,
-        },
-        {
-          name: 'Permanent Suspension Request Approved',
-          value: 0,
-        },
-      ],
-    });
-  }
+  let registrationRequestData = getDataFromResponse(
+    count,
+    registrationRequestMapper,
+    'hp_registration_requests'
+  );
+  let updationRequestData = getDataFromResponse(
+    count,
+    updationRequestMapper,
+    'hp_modification_requests'
+  );
+  // eslint-disable-next-line no-console
+  console.log('upd', updationRequestData);
+
+  let dashboard = {
+    'Registration Request': registrationRequestData,
+    'Updation Request': updationRequestData,
+  };
+
+  // if (loggedInUserType === 'NMC' || loggedInUserType === 'SMC') {
+  //   dashboard = Object.assign(dashboard, {
+  //     'Suspension Request': suspensionRequestData,
+  //   });
+  // }
+
+  // previous code below ->
+  // let blankDashboard = {
+  //   'Registration Request': [
+  //     {
+  //       name: 'Total Registration Requests',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Pending',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Verified',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Query Raised',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Rejected',
+  //       value: 0,
+  //     },
+  //   ],
+  //   'Updation Request': [
+  //     {
+  //       name: 'Total Updation Request',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Update Request Received',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Update Request Approved',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Query Raised on Update Request',
+  //       value: 0,
+  //     },
+  //     {
+  //       name: 'Update Request Rejected',
+  //       value: 0,
+  //     },
+  //   ],
+  // };
+
+  // if (loggedInUserType === 'NMC' || loggedInUserType === 'SMC') {
+  //   blankDashboard = Object.assign(blankDashboard, {
+  //     'Suspension Request': [
+  //       {
+  //         name: 'Total Suspension Request',
+  //         value: 0,
+  //       },
+  //       {
+  //         name: 'Temporary Suspension Request Received',
+  //         value: 0,
+  //       },
+  //       {
+  //         name: 'Temporary Suspension Approved',
+  //         value: 0,
+  //       },
+  //       {
+  //         name: 'Permanent Suspension Request Received',
+  //         value: 0,
+  //       },
+  //       {
+  //         name: 'Permanent Suspension Request Approved',
+  //         value: 0,
+  //       },
+  //     ],
+  //   });
+  // }
 
   const [showDashboard, setShowDashboard] = useState(true);
   const [showTable, setShowTable] = useState(false);
   const [showViewProfile, setShowViewPorfile] = useState(false);
 
-  const countResp = Object.values(count?.data);
-  const blankResp = Object.values(blankDashboard);
+  // const countResp = Object.values(count?.data);
+  // const blankResp = Object.values(blankDashboard);
 
-  const resultCountResp =
-    loggedInUserType === 'NMC' || loggedInUserType === 'SMC'
-      ? countResp['hp_registration_requests']
-          ?.concat(countResp['hp_modification_requests'])
-          ?.concat(countResp[2])
-      : countResp['hp_registration_requests']?.concat(countResp['hp_modification_requests']);
-  const resultblankResp =
-    loggedInUserType === 'NMC' || loggedInUserType === 'SMC'
-      ? blankResp['Registration Request']
-          ?.concat(blankResp['Updation Request'])
-          ?.concat(blankResp[2])
-      : blankResp['Registration Request']?.concat(blankResp['Updation Request']);
+  // const resultCountResp =
+  //   loggedInUserType === 'NMC' || loggedInUserType === 'SMC'
+  //     ? countResp['hp_registration_requests']
+  //         ?.concat(countResp['hp_modification_requests'])
+  //         ?.concat(countResp[2])
+  //     : countResp['hp_registration_requests']?.concat(countResp['hp_modification_requests']);
+  // const resultblankResp =
+  //   loggedInUserType === 'NMC' || loggedInUserType === 'SMC'
+  //     ? blankResp['Registration Request']
+  //         ?.concat(blankResp['Updation Request'])
+  //         ?.concat(blankResp[2])
+  //     : blankResp['Registration Request']?.concat(blankResp['Updation Request']);
 
-  // eslint-disable-next-line no-console
-  console.log(
-    'resultblankResp',
-    count,
-    countResp,
-    countResp['hp_registration_requests'],
-    resultblankResp,
-    resultCountResp
-  );
+  // // eslint-disable-next-line no-console
+  // console.log(
+  //   'resultblankResp 123',
+  //   count,
+  //   countResp,
+  //   countResp['hp_registration_requests'],
+  //   resultblankResp,
+  //   resultCountResp
+  // );
 
-  if (resultCountResp?.length > 0) {
-    for (let i = 0; i < resultCountResp?.length; i++) {
-      for (let j = 0; j < resultblankResp?.length; j++) {
-        // eslint-disable-next-line no-console
-        console.log('resultblankResp', resultblankResp, resultCountResp);
-        if (resultCountResp[i].name === resultblankResp[j].name) {
-          resultblankResp[j].value = resultCountResp[i].count;
-        }
-      }
-    }
+  // if (resultCountResp?.length > 0) {
+  //   for (let i = 0; i < resultCountResp?.length; i++) {
+  //     for (let j = 0; j < resultblankResp?.length; j++) {
+  //       // eslint-disable-next-line no-console
+  //       console.log('resultblankResp', resultblankResp, resultCountResp);
+  //       if (resultCountResp[i].name === resultblankResp[j].name) {
+  //         resultblankResp[j].value = resultCountResp[i].count;
+  //       }
+  //     }
+  //   }
+  // }
+
+  function getDataFromResponse(count, mapper, key) {
+    let data = [];
+    count?.data[key]?.forEach((request) => {
+      // eslint-disable-next-line no-console
+      console.log('1232', request);
+      let currObj = {
+        name: mapper[request['name']],
+        value: request['count'],
+      };
+      data.push(currObj);
+    });
+    return data;
   }
 
   function handleBreadCrumClick(event) {
@@ -162,6 +217,8 @@ export default function Dashboard() {
     setShowDashboard(false);
     setShowTable(true);
     setShowViewPorfile(false);
+    // eslint-disable-next-line no-console
+    console.log('item,', item);
     verboseLog('item', item);
     // setShowTable({ show: true, value: item.id, count: item.value })
   };
@@ -218,7 +275,7 @@ export default function Dashboard() {
           </Typography>
           <Box sx={{ width: '100%' }}>
             <Box display="flex" flexWrap="wrap" gap={1}>
-              {Object.entries(blankDashboard).map((element) => {
+              {Object.entries(dashboard).map((element) => {
                 return (
                   <>
                     <Typography flex="1 0 100%">{element[0]}</Typography>
