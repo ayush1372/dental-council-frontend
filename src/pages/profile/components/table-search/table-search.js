e23e43a809f9fa05158e460758ddc9134b31ebf5
Somnath Jadhav <somnath.jadhav@lntinfotech.com>
2023-03-11 17:22:09 +0530

    added filter functionality for track application

--- src/pages/profile/components/table-search/table-search.js
+++ src/pages/profile/components/table-search/table-search.js
@@ -1,6 +1,6 @@
-import SearchIcon from '@mui/icons-material/Search';
-import { Box, Grid, IconButton, InputAdornment } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+// import SearchIcon from '@mui/icons-material/Search';
+import { Box, Grid } from '@mui/material';
+// import { useTheme } from '@mui/material/styles';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -17,10 +17,13 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
   exportData?.map((data) => {
     applicationData.push(data.application_type_name);
   });
+  // eslint-disable-next-line no-console
+  console.log('in table serach props valeus ', exportData, trackApplication);
   const loggedInUserType = useSelector((state) => state.common.loggedInUserType);
   const { councilNames } = useSelector((state) => state.common);
+  const profileId = useSelector((state) => state.loginReducer.loginData.data.profile_id);
   const dispatch = useDispatch();
-  const theme = useTheme();
+  // const theme = useTheme();
   let trackData = {
     pageNo: 1,
     offset: 10,
@@ -46,7 +49,22 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
       FilterId: '',
     },
   });
-  const onClickFilterButtonHandler = (data) => {
+  const onClickSearchButtonHandler = (data) => {
+    // eslint-disable-next-line no-console
+    console.log('on click of search button', getValues().FilterId, getValues().FilterValue);
+    if (
+      trackApplication &&
+      getValues().FilterId !== '' &&
+      getValues().FilterId !== undefined &&
+      getValues().FilterId !== null &&
+      getValues().FilterValue !== '' &&
+      getValues().FilterValue !== undefined &&
+      getValues().FilterValue !== null
+    ) {
+      trackData.search = getValues().FilterId;
+      trackData.value = getValues().FilterValue;
+      dispatch(getDoctorTrackApplicationData(profileId, trackData));
+    }
     searchParams(data);
     reset({
       filterByName: '',
@@ -57,18 +75,18 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
     });
   };
 
-  const onClickSearchButtonHandler = (data) => {
-    trackData.search = data.search;
-    searchParams(data);
-    dispatch(getDoctorTrackApplicationData(trackData));
-    reset({
-      filterByName: '',
-      filterByRegNo: '',
-      registrationCouncil: '',
-      RegistrationCouncilId: '',
-      search: '',
-    });
-  };
+  // const onClickSearchButtonHandler = (data) => {
+  //   trackData.search = data.search;
+  //   searchParams(data);
+  //   dispatch(getDoctorTrackApplicationData(trackData));
+  //   reset({
+  //     filterByName: '',
+  //     filterByRegNo: '',
+  //     registrationCouncil: '',
+  //     RegistrationCouncilId: '',
+  //     search: '',
+  //   });
+  // };
 
   return (
     <Box data-testid="table-search" mb={2}>
@@ -79,7 +97,7 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
           xs={12}
           mb={{ xs: 1, sm: 0 }}
         >
-          <TextField
+          {/* <TextField
             sx={{ mt: 1 }}
             data-testid="freesearch"
             inputProps={{ maxLength: 100 }}
@@ -110,7 +128,7 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
                 </InputAdornment>
               ),
             }}
-          />
+          /> */}
         </Grid>
 
         <Grid item md={trackApplication ? 7 : activateLicence ? 8 : 10} xs={12}>
@@ -126,7 +144,6 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
                     clearErrors={clearErrors}
                     {...register('Filter')}
                     onChange={(currentValue) => {
-                      alert(currentValue.id);
                       setValue('FilterId', currentValue.id);
                     }}
                   />
@@ -239,9 +256,9 @@ export function TableSearch({ trackApplication, activateLicence, searchParams, e
                     },
                   }}
                   variant="contained"
-                  onClick={handleSubmit(onClickFilterButtonHandler)}
+                  onClick={handleSubmit(onClickSearchButtonHandler)}
                 >
-                  Filter
+                  Search
                 </Button>
               </Grid>
             )}
--- src/store/actions/doctor-user-profile-actions.js
+++ src/store/actions/doctor-user-profile-actions.js
@@ -157,17 +157,51 @@ export const getUserProfileImage = (hp_profile_id, file) => async (dispatch) => 
 };
 
 export const getDoctorTrackApplicationData = (doctor_profile_id, trackData) => async (dispatch) => {
+  // let path = '';
+  // if (trackData.search !== undefined && trackData.search !== null && trackData.search !== '') {
+  //   path += '&search=' + trackData.search;
+  // }
+  // if (trackData.value !== undefined && trackData.value !== null && trackData.value !== '') {
+  //   path += '&value=' + trackData.value;
+  // }
+  // // eslint-disable-next-line no-console
+  // console.log('path is ===', path);
+
   let path = '';
   if (trackData.search !== undefined && trackData.search !== null && trackData.search !== '') {
-    path += '&search=' + trackData.search;
+    if (path === '') {
+      path += 'search=' + trackData.search;
+    } else {
+      path += '&search=' + trackData.search;
+    }
   }
+  if (trackData.value !== undefined && trackData.value !== null && trackData.value !== '') {
+    if (path === '') {
+      path += 'value=' + trackData.value;
+    } else {
+      path += '&value=' + trackData.value;
+    }
+  }
+
+  if (trackData.pageNo !== undefined && trackData.pageNo !== null && trackData.pageNo !== '') {
+    path += '&pageNo=' + trackData.pageNo;
+  }
+  if (trackData.offset !== undefined && trackData.offset !== null && trackData.offset !== '') {
+    path += '&size=' + trackData.offset;
+  }
+  // eslint-disable-next-line no-console
+  console.log('final url is ', path);
   return await new Promise((resolve, reject) => {
     useAxiosCall({
       method: GET,
       url: `${API.DoctorUserProfileData.trackApplicationData.replace(
         '{healthProfessionalId}',
         doctor_profile_id
-      )}?pageNo=${trackData.pageNo}&offset=${trackData.offset}${path}`,
+      )}?${path}`,
+      // url: `${API.DoctorUserProfileData.trackApplicationData.replace(
+      //   '{healthProfessionalId}',
+      //   doctor_profile_id
+      // )}?pageNo=${trackData.pageNo}&offset=${trackData.offset}`,
       // url: API.DoctorUserProfileData.trackApplicationData.replace(
       //   '{healthProfessionalId}',
       //   doctor_profile_id
