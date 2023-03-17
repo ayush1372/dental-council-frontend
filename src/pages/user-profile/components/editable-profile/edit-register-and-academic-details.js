a3a69b1f3f38d9c5cebd5325fddbb1a17ad6f24d
Ashvini <ashvini.deshmukh@lntinfotech.com>
2023-03-17 18:02:16 +0530

    added conditional statement in id

--- src/pages/user-profile/components/editable-profile/edit-register-and-academic-details.js
+++ src/pages/user-profile/components/editable-profile/edit-register-and-academic-details.js
@@ -138,7 +138,9 @@ const EditRegisterAndAcademicDetails = ({ handleNext, handleBack }) => {
     let updatedObj = [];
     if (qualification?.length > 0) {
       updatedObj = qualification?.map((q) => ({
-        id: qualification_detail_response_tos[0].id ? qualification_detail_response_tos[0].id : '',
+        id: qualification_detail_response_tos[0]?.id
+          ? qualification_detail_response_tos[0]?.id
+          : '',
         country: countriesList.find((x) => x.id === q?.country),
         course: coursesList.data?.find((x) => x.id === q?.qualification),
         university: universitiesList.data?.find((x) => x.id === q?.university),
