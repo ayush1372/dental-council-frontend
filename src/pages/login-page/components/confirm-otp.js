8b99d6fade16a6040a3e0ce3afedc02d653246a3
abdulbasith1410 <ahmedabdul.basith@lntinfotech.com>
2023-04-18 16:45:30 +0530

    Comments resolved

--- src/pages/login-page/components/confirm-otp.js
+++ src/pages/login-page/components/confirm-otp.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 import BookOnlineIcon from '@mui/icons-material/BookOnline';
-// import BookOnlineIcon from '@mui/icons-material/BookOnline';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
