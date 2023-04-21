d991864efd1ae0b526151fbf7704eacefa8cca1d
mahalingam <mahalingam.vs@lntinfotech.com>
2023-04-21 14:32:07 +0530

    CS-1688 Issue Fixes

--- src/pages/login-page/components/confirm-otp.js
+++ src/pages/login-page/components/confirm-otp.js
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import BookOnlineIcon from '@mui/icons-material/BookOnline';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box, Typography } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 
+import IconVerified from '../../../assets/images/ico-verified.svg';
 import { encryptData } from '../../../helpers/functions/common-functions';
 import SuccessModalPopup from '../../../shared/common-modals/success-modal-popup';
 import { OtpForm } from '../../../shared/otp-form/otp-component';
@@ -131,7 +133,8 @@ const ConfirmOTP = ({ handleConfirmOTP, otpData, resetStep, handlePasswordSetup 
 
   return (
     <Box p={4} bgcolor="white.main" boxShadow="4">
-      {otpData.page === 'doctorConstantDetailsPage' || otpData.page === 'forgetUserName' ? (
+      {(otpData.page === 'doctorConstantDetailsPage' && otpData.type === 'sms') ||
+      otpData.page === 'forgetUserName' ? (
         <>
           <Box display={'flex'} justifyContent="flex-end" mb={3}>
             {otpData.page === 'forgetUserName' && (
@@ -152,6 +155,20 @@ const ConfirmOTP = ({ handleConfirmOTP, otpData, resetStep, handlePasswordSetup 
             OTP Authentication
           </Typography>
         </>
+      ) : otpData.page === 'doctorConstantDetailsPage' && otpData.type === 'email' ? (
+        <Box display={'flex'} mb={3} justifyContent="space-between">
+          <Box display={'flex'} alignItems="center">
+            <Box mr={1}>
+              <img width="20px" height="20px" src={IconVerified} alt="verified icon" />
+            </Box>
+            <Box display="flex">
+              <Typography variant="h2">Email Sent Successfully</Typography>
+            </Box>
+          </Box>
+          <Box flex-grow={1}>
+            <CloseIcon onClick={otpData.handleClose} />
+          </Box>
+        </Box>
       ) : (
         <Typography variant="h2" component="div">
           Confirm OTP
@@ -170,14 +187,18 @@ const ConfirmOTP = ({ handleConfirmOTP, otpData, resetStep, handlePasswordSetup 
           {otpData?.page === 'doctorConstantDetailsPage' ? (
             <Typography textAlign="center">
               {otpData.page === 'doctorConstantDetailsPage' && otpData?.type === 'sms'
-                ? `Please enter the OTP sent on your Mobile Number XXXXXX${otpData?.contact?.slice(
+                ? `Please enter the OTP sent on your Mobile Number XXXXXX${otpData?.contact.slice(
                     -4
                   )}.`
                 : otpData.page === 'doctorConstantDetailsPage' &&
-                  otpData?.type === 'email' &&
-                  `Please enter the OTP sent on your Email ID XXXXXX${otpData?.contact?.slice(
-                    -12
-                  )}`}
+                  otpData?.type === 'email' && (
+                    <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+                      <CircularProgress color="secondary" />
+                      <Typography textAlign="center" mt={2}>
+                        Waiting for Conformation
+                      </Typography>
+                    </Box>
+                  )}
             </Typography>
           ) : otpData?.page === 'forgotPasswordPage' ? (
             <Typography variant="body" textAlign="center">
@@ -215,9 +236,13 @@ const ConfirmOTP = ({ handleConfirmOTP, otpData, resetStep, handlePasswordSetup 
               Verification code
             </Typography>
           )}
-          <Box display={'flex'} justifyContent="space-between">
-            {otpform}
-          </Box>
+          {(otpData.page === 'doctorConstantDetailsPage' ||
+            otpData.page === 'forgotPasswordPage') &&
+            otpData?.type !== 'email' && (
+              <Box display={'flex'} justifyContent="center">
+                {otpform}
+              </Box>
+            )}
         </Box>
         {otpData?.page === 'forgetUserName' ? (
           <Box display={'flex'} justifyContent="center">
@@ -237,33 +262,36 @@ const ConfirmOTP = ({ handleConfirmOTP, otpData, resetStep, handlePasswordSetup 
             </Button>
           </Box>
         ) : (
-          <Box mt={3} textAlign="center">
-            <Button
-              onClick={() => {
-                resetStep(0);
-              }}
-              variant="contained"
-              color="grey"
-              sx={{
-                mr: 2,
-              }}
-            >
-              Cancel
-            </Button>
-            <Button
-              size="medium"
-              variant="contained"
-              sx={{
-                backgroundColor: 'secondary.lightOrange',
-                '&:hover': {
+          (otpData.page === 'doctorConstantDetailsPage' || otpData.page === 'forgotPasswordPage') &&
+          otpData?.type !== 'email' && (
+            <Box mt={3} textAlign="center">
+              <Button
+                onClick={() => {
+                  resetStep(0);
+                }}
+                variant="contained"
+                color="grey"
+                sx={{
+                  mr: 2,
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                size="medium"
+                variant="contained"
+                sx={{
                   backgroundColor: 'secondary.lightOrange',
-                },
-              }}
-              onClick={onHandleVerify}
-            >
-              {t('Continue')}
-            </Button>
-          </Box>
+                  '&:hover': {
+                    backgroundColor: 'secondary.lightOrange',
+                  },
+                }}
+                onClick={onHandleVerify}
+              >
+                {t('Continue')}
+              </Button>
+            </Box>
+          )
         )}
       </Box>
       {changeUserData && (
