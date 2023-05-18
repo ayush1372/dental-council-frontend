93a9457d6c90c7238b5e8e9ad01a3ba5f0a27dd4
abdulbasith1410 <ahmedabdul.basith@lntinfotech.com>
2023-05-18 14:38:41 +0530

    captcha validation

--- src/shared/captcha-component/captcha-component.js
+++ src/shared/captcha-component/captcha-component.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { Grid, Typography, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -17,7 +18,10 @@ const CaptchaComponent = ({ captchaResult }) => {
   const [error, setError] = useState();
   const dispatch = useDispatch();
   const theme = useTheme();
-
+  const { register, getValues, setValue } = useForm({
+    mode: 'onChange',
+    defaultValues: { anwser: anwser },
+  });
   const reloadCaptcha = () => {
     setAnwser('');
     if (captchaEnabledFlag?.data) {
@@ -31,11 +35,6 @@ const CaptchaComponent = ({ captchaResult }) => {
     setAnwser('');
   }, [generateCaptcha?.transaction_id]);
 
-  const handleChange = (e) => {
-    setAnwser(e.target.value);
-    captchaResult(e.target.value);
-  };
-
   const onFocusChange = (e) => {
     e.preventDefault();
     const userResponse = anwser;
@@ -45,6 +44,7 @@ const CaptchaComponent = ({ captchaResult }) => {
     } else {
       setError(null);
     }
+    userResponse.length > 2 ? setError('Enter captcha answer') : setError('');
   };
 
   return (
@@ -88,9 +88,28 @@ const CaptchaComponent = ({ captchaResult }) => {
                     // className={`${!error ? 'text-captcha' : 'text-captcha-danger'}`}
                     name="anwser"
                     placeholder="Enter Answer"
-                    value={anwser}
-                    onBlur={onFocusChange}
-                    onChange={handleChange}
+                    defaultValue={getValues().anwser}
+                    required
+                    {...register('anwser', {
+                      required: 'Please Enter Valid Name',
+                      pattern: {
+                        value: `^[0-9]$`,
+                        message: 'Please Enter Valid Name',
+                      },
+                      maxLength: {
+                        value: 2,
+                      },
+                      onChange: (event) => {
+                        const captchaValue = event.target.value.replace(/[^0-9]/g, '');
+                        setValue('anwser', captchaValue);
+                        setAnwser(captchaValue);
+                        captchaResult(captchaValue);
+                        captchaValue.length > 2 ? setError('Enter captcha answer') : setError('');
+                      },
+                      onBlur: (event) => {
+                        onFocusChange(event);
+                      },
+                    })}
                   />
                 </Grid>
               </Grid>
