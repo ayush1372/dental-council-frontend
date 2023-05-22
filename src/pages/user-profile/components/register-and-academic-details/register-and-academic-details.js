b0488320ea009955d466559b6182276a08f835ae
abdulbasith1410 <ahmedabdul.basith@lntinfotech.com>
2023-05-22 18:47:59 +0530

    Query raised

--- src/pages/user-profile/components/register-and-academic-details/register-and-academic-details.js
+++ src/pages/user-profile/components/register-and-academic-details/register-and-academic-details.js
@@ -2,11 +2,21 @@ import { Box } from '@mui/material';
 
 import EditRegisterAndAcademicDetails from '../editable-profile/edit-register-and-academic-details';
 import ReadRegisterAndAcademicDetails from '../read-register-and-academic-details/read-register-and-academic-details';
-const RegisterAndAcademicDetails = ({ isReadMode, handleNext, handleBack, loggedInUserType }) => {
+const RegisterAndAcademicDetails = ({
+  isReadMode,
+  handleNext,
+  handleBack,
+  loggedInUserType,
+  selectedDataIndex,
+}) => {
   return (
     <Box mt={1} py={4}>
       {isReadMode && loggedInUserType !== 'SMC' && (
-        <ReadRegisterAndAcademicDetails handleBack={handleBack} isReadMode={isReadMode} />
+        <ReadRegisterAndAcademicDetails
+          handleBack={handleBack}
+          isReadMode={isReadMode}
+          selectedDataIndex={selectedDataIndex}
+        />
       )}
       {!isReadMode && (
         <EditRegisterAndAcademicDetails handleNext={handleNext} handleBack={handleBack} />
