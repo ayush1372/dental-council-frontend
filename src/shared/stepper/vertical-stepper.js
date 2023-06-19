dca17c33245d8447b912b56d4cda0fca55ce8575
Somnath Jadhav <Somnath.Jadhav@lntinfotech.com>
2023-06-19 09:51:32 +0530

    added work details map Ui and time issue fixed

--- src/shared/stepper/vertical-stepper.js
+++ src/shared/stepper/vertical-stepper.js
@@ -35,8 +35,15 @@ export default function VerticalLinearStepper() {
   };
   const getTime = (date) => {
     const dateObj = new Date(date);
-
-    return ` ${dateObj.getHours()}:${dateObj.getMinutes()}`;
+    let ans = dateObj.getMinutes();
+    let minute;
+    if (ans >= 0 && ans <= 9) {
+      minute = '0' + ans;
+    } else {
+      minute = ans;
+    }
+    return ` ${dateObj.getHours()}:${minute}`;
+    // return ` ${dateObj.getHours()}:${dateObj.getMinutes()}`;
   };
 
   const theme = useTheme();
