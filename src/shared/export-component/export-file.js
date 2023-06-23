1b53ba74d0056257871d6e2a2db8674f209fd96a
mahalingam <mahalingam.vs@lntinfotech.com>
2023-06-23 12:02:55 +0530

    Cleanup changes

--- src/shared/export-component/export-file.js
+++ src/shared/export-component/export-file.js
@@ -1,15 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import {
-  Grid,
-  IconButton,
-  // List,
-  // ListItem,
-  // ListItemButton,
-  // ListItemText,
-  // Popover,
-} from '@mui/material';
+import { Grid, IconButton } from '@mui/material';
 import Excel from 'exceljs';
 import { saveAs } from 'file-saver';
 import moment from 'moment';
@@ -24,7 +16,6 @@ import { userActionId, workSheetTheme } from '../../../src/helpers/functions/com
 import { verboseLog } from '../../config/debug';
 
 const ExportFiles = ({ exportData, flag }) => {
-  // const [anchorEl, setAnchorEl] = useState(null);
   const [csvData, setCsvData] = useState([]);
   const [docType, setDocType] = useState();
   const [columns, setColumns] = useState([]);
@@ -94,14 +85,6 @@ const ExportFiles = ({ exportData, flag }) => {
     setCsvData(data);
   };
 
-  // const handleClick = (event) => {
-  //   setAnchorEl(event.currentTarget);
-  // };
-
-  // const handleClose = () => {
-  //   setAnchorEl(null);
-  // };
-
   useEffect(() => {
     if (csvData?.length > 0) {
       saveExcel();
@@ -147,10 +130,6 @@ const ExportFiles = ({ exportData, flag }) => {
         const buf = await workbook.xlsx.writeBuffer();
         saveAs(new Blob([buf]), `${fileName}.xlsx`);
       }
-      // if (docType === 'csv') {
-      //   const buf = await workbook.csv.writeBuffer();
-      //   saveAs(new Blob([buf]), `${fileName}.csv`);
-      // }
     } catch (error) {
       verboseLog('<<<ERRROR>>>', error);
       verboseLog('Something Went Wrong', error.message);
@@ -161,8 +140,6 @@ const ExportFiles = ({ exportData, flag }) => {
     }
   };
 
-  // const open = Boolean(anchorEl);
-  // const id = open ? 'table-search-popover' : undefined;
   return (
     <Grid item md={1} xs={12} data-testid="exportButton">
       <IconButton
@@ -190,47 +167,6 @@ const ExportFiles = ({ exportData, flag }) => {
           }}
         />
       </IconButton>
-      {/*
-      As per our Discussion commented the CSV export and xlsx export is triggered the event from export button. to make it as consistency across building Blocks of ABDM.
-      CC: @Govind, Ashvini , Sadik, 
-      Author: Mahalingam V S
-       <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-      >
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton onClick={handleClose}>
-              <ListItemText
-                primary="Export as xlsx"
-                onClick={() => {
-                  onExportClick('xlsx');
-                }}
-              />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton onClick={handleClose}>
-              <ListItemText
-                primary="Export as csv"
-                onClick={() => {
-                  onExportClick('csv');
-                }}
-              />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Popover> */}
     </Grid>
   );
 };
