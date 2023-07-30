9b4bdb2cd679987e1df78e638199f832778d65ee
Laxman Sutar <Laxman.Sutar@lntinfotech.com>
2023-07-30 11:52:26 +0530

    footer design updated with social media

--- src/ui/layout/footer/footer.js
+++ src/ui/layout/footer/footer.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Container, Divider, Grid } from '@mui/material';
 
 import { AbdmHealthRecords } from './components/abdm-health-records/abdm-health-records';
 import { Address } from './components/address/address';
@@ -6,20 +6,19 @@ import { Copyright } from './components/copyright/copyright';
 import { ImportantLinks } from './components/important-links/important-links';
 import { Policies } from './components/policies/policies';
 
-import styles from './footer.module.scss';
-
 export const Footer = () => (
-  <>
-    <Box className={styles.footerWrapper}>
+  <Box>
+    <Divider variant="fullWidth" component="div" sx={{ boxShadow: '1' }} />
+    <Box bgcolor="primary.main" color="white.main" py={4}>
       <Container>
-        <Grid container columnSpacing={{ xs: 2 }}>
-          <Grid item xs={12} sm={6} md={3}>
+        <Grid container columnSpacing={2}>
+          <Grid item xs={12} sm={6} md={3} mb={{ xs: 3, md: 0 }}>
             <Address />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} mb={{ xs: 3, md: 0 }}>
             <ImportantLinks />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3} mb={{ xs: 3, md: 0 }}>
             <Policies />{' '}
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
@@ -29,5 +28,5 @@ export const Footer = () => (
       </Container>
     </Box>
     <Copyright />
-  </>
+  </Box>
 );
