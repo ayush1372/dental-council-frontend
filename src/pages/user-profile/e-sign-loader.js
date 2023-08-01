ca9f52cf48e82f1aa0c1f1a0b7b6829c211de3a5
mahalingam <mahalingam.vs@lntinfotech.com>
2023-08-01 13:16:14 +0530

    E-sign Loader changes and action id changes for the NMC user

---
+++ src/pages/user-profile/e-sign-loader.js
@@ -0,0 +1,29 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { Box, Grid, Typography } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const ConfirmEsignProcess = ({ handleClose }) => {
+  return (
+    <Box p={3} bgcolor="white.main" boxShadow="4">
+      <Grid>
+        <Box display={'flex'} justifyContent="flex-end">
+          <CloseIcon sx={{ cursor: 'pointer' }} onClick={handleClose} />
+        </Box>
+        <Box textAlign={'center'}>
+          <Typography variant="h2">E-sign Verfication</Typography>
+        </Box>
+      </Grid>
+
+      <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+        <CircularProgress color="primary" />
+        <Typography textAlign="center" mt={2}>
+          You have been re-dircted to E-sign portal for verfication process.
+        </Typography>
+        <Typography textAlign="center" mt={2}>
+          Awaiting E-sign Confirmation
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+export default ConfirmEsignProcess;
