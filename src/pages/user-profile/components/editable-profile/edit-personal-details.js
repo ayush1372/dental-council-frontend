8cb5c18df4defa1c519aca1b50395dfbc02368ca
Ashvini <ashvini.deshmukh@lntinfotech.com>
2023-08-07 20:13:43 +0530

    district is mandatory in both the cases

--- src/pages/user-profile/components/editable-profile/edit-personal-details.js
+++ src/pages/user-profile/components/editable-profile/edit-personal-details.js
@@ -270,6 +270,14 @@ const EditPersonalDetails = ({ handleNext, setIsReadMode, validDetails, setValid
   useEffect(() => {
     if (selectedState !== undefined) {
       fetchDistricts(selectedState);
+      if (
+        personalDetails?.kyc_address?.district?.iso_code !== '' ||
+        personalDetails?.kyc_address?.district?.iso_code !== undefined ||
+        personalDetails?.kyc_address?.district?.iso_code !== null ||
+        isSameAddress
+      ) {
+        clearErrors('District', '');
+      }
     }
   }, [selectedState]);
 
@@ -1200,11 +1208,9 @@ const EditPersonalDetails = ({ handleNext, setIsReadMode, validDetails, setValid
             <Grid item xs={12} md={4}>
               <Typography variant="subtitle2" color="inputTextColor.main">
                 District
-                {!isSameAddress && (
-                  <Typography component="span" color="error.main">
-                    *
-                  </Typography>
-                )}
+                <Typography component="span" color="error.main">
+                  *
+                </Typography>
               </Typography>
               {isSameAddress || (work_flow_status_id === 3 && getQueryRaised('District')) ? (
                 <TextField
@@ -1226,20 +1232,13 @@ const EditPersonalDetails = ({ handleNext, setIsReadMode, validDetails, setValid
                   //       : '',
                   //   },
                   // }}
-                  required={isSameAddress ? false : true}
-                  error={!isSameAddress ? errors.District?.message : ''}
+                  required={true}
+                  error={errors.District?.message}
                   fullWidth
                   value={getDistrictData(getValues()?.District)?.name}
-                  {...register(
-                    'District',
-                    !isSameAddress
-                      ? {
-                          required: 'District is required',
-                        }
-                      : {
-                          required: false,
-                        }
-                  )}
+                  {...register('District', {
+                    required: 'District is required',
+                  })}
                   // {...register('District', {
                   //   // required: 'District is required',
                   // })}
@@ -1255,7 +1254,7 @@ const EditPersonalDetails = ({ handleNext, setIsReadMode, validDetails, setValid
                   //     : '',
                   // }}
                   fullWidth
-                  error={!isSameAddress ? errors.District?.message : ''}
+                  error={errors.District?.message}
                   name="District"
                   defaultValue={
                     isSameAddress
@@ -1274,17 +1273,10 @@ const EditPersonalDetails = ({ handleNext, setIsReadMode, validDetails, setValid
                       ? getQueryRaised('District')
                       : false
                   }
-                  required={isSameAddress ? false : true}
-                  {...register(
-                    'District',
-                    !isSameAddress
-                      ? {
-                          required: 'District is required',
-                        }
-                      : {
-                          required: false,
-                        }
-                  )}
+                  required={true}
+                  {...register('District', {
+                    required: 'District is required',
+                  })}
                   options={createSelectFieldData(districtsList, 'iso_code')}
                   MenuProps={{
                     style: {
